40e65c6caa51e52bd8b617a6c9a39d764f61059d
DESKTOP-KP2SU33\haeun <hhaeun5419@gmail.com>
2021-12-29 13:34:30 +0900

    단일 그래프로 변경-pyQt5

--- 2dMapping.py
+++ 2dMapping.py
@@ -14,8 +14,6 @@ class MyMplCanvas(FigureCanvas):
         fig = Figure(figsize=(width, height), dpi=dpi)
 
         self.axes = fig.add_subplot(211, xlim=(0, 50), ylim=(0, 1024))
-        self.axes2 = fig.add_subplot(212, xlim=(0, 50), ylim=(0, 600))
-
         self.compute_initial_figure()
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
@@ -36,10 +34,6 @@ def file_read(f):
     angles = np.array(angles)
     distances = np.array(distances)
     return angles, distances
-
-
-
-
 
 class AnimationWidget(QWidget):
     def __init__(self):
@@ -69,15 +63,9 @@ class AnimationWidget(QWidget):
         plt.grid(True)
         plt.show()
 
-
-
         self.x = np.arange(50)
         self.y = np.ones(50, dtype=np.float64) * np.nan
         self.line, = self.canvas.axes.plot(self.x, self.y, animated=True, lw=2)
-
-        self.x2 = np.arange(50)
-        self.y2 = np.ones(50, dtype=np.float64) * np.nan
-        self.line2, = self.canvas.axes2.plot(self.x2, self.y2, animated=True, color='red', lw=2)
 
     def update_line(self, i):
         y = random.randint(0, 1024)
@@ -89,21 +77,11 @@ class AnimationWidget(QWidget):
         print(self.y)
         return [self.line]
 
-    def update_line2(self, i):
-        y2 = random.randint(0, 510)
-        old_y2 = self.line2.get_ydata()
-        new_y2 = np.r_[old_y2[1:], y2]
-        self.line2.set_ydata(new_y2)
-        return [self.line2]
-        # self.line.set_ydata(y)
-
     def on_start(self):
         self.ani = animation.FuncAnimation(self.canvas.figure, self.update_line, blit=True, interval=25)
-        self.ani2 = animation.FuncAnimation(self.canvas.figure, self.update_line2, blit=True, interval=25)
 
     def on_stop(self):
         self.ani._stop()
-        self.ani2._stop()
 
 
 if __name__ == "__main__":
